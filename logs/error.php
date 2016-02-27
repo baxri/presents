b2fb0e80ebf620da15f97111c925d1bf3e96a2cc
gio <giorgi.bibilashvili89@gmail.com>
2016-02-27 20:33:32 +0400

    --

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-02-24 18:44:10 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-02-24T18:44:10+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
