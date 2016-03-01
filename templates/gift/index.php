f694b75d45fc210570c4927da442527c255a5ca8
gio <giorgi.bibilashvili89@gmail.com>
2016-03-02 00:33:18 +0400

    backand

--- templates/gift/index.php
+++ templates/gift/index.php
@@ -42,14 +42,25 @@
     <jdoc:include type="message" />
     <jdoc:include type="component" />
 
-    <div class="clear"></div>
 
-    <footer class="footer">
-        <div class="footer-container">
-            <jdoc:include type="modules" name="footer_menu" />
-        </div>
-    </footer>
+    <?php if ($this->countModules( 'footer_menu' )) : ?>
+        <div class="clear"></div>
+        <footer class="footer">
+            <div class="footer-container">
+                <jdoc:include type="modules" name="footer_menu" />
+            </div>
+        </footer>
 
+        <footer class="footer footer2">
+            <div class="footer-container footer-container2">
+                ff
+            </div>
+            <div class="footer-container">
+                2016
+            </div>
+        </footer>
+
+    <?php endif; ?>
 
 </body>
 </html>
